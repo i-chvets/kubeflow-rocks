1c399eae75bcfd5e006457969178d11e89b9e31f
Ivan Chvets <ivan.chvets@canonical.com>
2023-04-29 05:30:39 -0400

    feat: added jira url

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -9,7 +9,6 @@ set -e
 sudo apt update
 sudo apt install -y make
 sudo apt install -y runc containerd
-#sudo apt install -y docker.io
 sudo apt install -y zip
 sudo snap install jq
 sudo snap install yq
