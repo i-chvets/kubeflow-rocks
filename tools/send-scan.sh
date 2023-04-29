05176e9fc5464974c7f493172717a6c856bdcd27
Ivan Chvets <ivan.chvets@canonical.com>
2023-04-29 05:32:26 -0400

    feat: added error exit

--- tools/send-scan.sh
+++ tools/send-scan.sh
@@ -13,13 +13,13 @@ if [ -z $DIR ]
 then
     echo "ERROR: Directory with scan results is not specified."
     echo "Usage: send-scan.sh <directory-with-scan-results> <jira-url>"
-    exit
+    exit 1
 fi
 if [ -z $JIRA_URL ]
 then
 echo "ERROR: URL is not specified."
     echo "Usage: send-scan.sh <directory-with-scan-results> <jira-url>"
-    exit
+    exit 1
 fi
 
 # get script that sends scan results from Kubeflow CI repo
