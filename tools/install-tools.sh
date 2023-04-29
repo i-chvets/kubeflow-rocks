8acad6843ad0638e266c751e5e3e1ae831fe5631
Ivan Chvets <ivan.chvets@canonical.com>
2023-04-29 04:50:15 -0400

    feat: update workflow

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -6,14 +6,13 @@
 #
 set -e
 
-#sudo apt update
-#sudo apt install -y make
-#sudo apt install -y runc containerd
-#sudo apt install -y docker.io
+sudo apt install -y make
+sudo apt install -y runc containerd
+sudo apt install -y docker.io
 sudo apt install -y zip
 sudo snap install jq
 sudo snap install yq
-#sudo snap install go --classic
+sudo snap install go --classic
 sudo snap install docker
 sudo snap install --edge --devmode skopeo
 sudo groupadd -f docker
